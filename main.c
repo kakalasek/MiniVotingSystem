e2403dd1d0e515c3a5a4b4440685cf651f8e053d
Typicek <carotkacz333@seznam.cz>
2023-05-03 11:03:09 +0200

    Input fixed

--- main.c
+++ main.c
@@ -1,14 +1,31 @@
 #include <stdio.h>
 #include <string.h>
 
+void stringValidation(int maxLength, char *string);
+
 int main(){
-    char validationNumber[14];
-    char candidateNumber[3];
+    char validationNumber[30];
+    char candidateNumber[10];
 
     printf("Enter your validation number:\n");
-    scanf("%13s", validationNumber);
+    fgets(validationNumber, sizeof(validationNumber), stdin);
+    if(validationNumber[strlen(validationNumber)] == '\n'){
+        getchar();
+    }
+    stringValidation(14, validationNumber);
     printf("\nEnter the number of your candidate:\n");
-    scanf("%2s", candidateNumber);
+    fgets(candidateNumber, sizeof(candidateNumber), stdin);
+    if(candidateNumber[strlen(candidateNumber)] == '\n'){
+        getchar();
+    }
+    stringValidation(3, candidateNumber);
 
     return 0;
+}
+
+void stringValidation(int maxLength, char *string){
+    if(strlen(string) > maxLength){
+        printf("Your input is invalid!\n");
+        exit(1);
+    }
 }
